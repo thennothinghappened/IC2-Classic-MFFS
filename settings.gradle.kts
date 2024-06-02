50e759f703fa23d5ba039cf8444a21f6a7b854b9
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-06-02 13:37:17 +1000

    build: Migrate to Kotlin 2!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,5 +11,5 @@ pluginManagement {
 
 plugins {
     id("net.minecraftforge.gradle") version "6.0.+" apply false
-    kotlin("jvm") version "1.9.23" apply false
+    kotlin("jvm") version "2.0.0" apply false
 }
