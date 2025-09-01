547ea0fe7187433aaf48d97e29c91ea9978d13e7
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-09-01 21:38:45 +1000

    refactor: add simpler serialization methods

--- src/main/java/mods/orca/mffs/blocks/utils/TileEntityUtils.kt
+++ src/main/java/mods/orca/mffs/blocks/utils/TileEntityUtils.kt
@@ -1,5 +1,8 @@
 package mods.orca.mffs.blocks.utils
 
+import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.runCatching
+import mods.orca.mffs.MFFSMod
 import mods.orca.mffs.utils.getTagOrNull
 import net.minecraft.nbt.NBTBase
 import net.minecraft.tileentity.TileEntity
@@ -18,4 +21,20 @@ var TileEntity.serializedStateOrNull: NBTBase?
         } else {
             tileData.removeTag(NBT_SERIALIZED_STATE_KEY)
         }
+    }
+
+/**
+ * An entity which has some type-safe NBT data.
+ */
+interface HasNbtState<T>
+
+inline fun <C, reified T> C.serialize(state: T)
+        where C : TileEntity, C : HasNbtState<T> {
+    serializedStateOrNull = MFFSMod.nbt.encode(state)
+}
+
+inline fun <C, reified T> C.deserialize(): Result<T, Throwable>?
+        where C : TileEntity, C : HasNbtState<T> =
+    serializedStateOrNull?.let { it ->
+        runCatching { MFFSMod.nbt.decode<T>(it) }
     }
