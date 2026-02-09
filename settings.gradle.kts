8557e243dea59546e8d42cd99d25ebc195d22af0
orca <35243139+thennothinghappened@users.noreply.github.com>
2026-02-09 18:53:13 +1100

    chore: update kotlin to 2.3.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,8 +20,8 @@ pluginManagement {
     plugins {
         id("net.minecraftforge.gradle") version "6.0.+"
         id("com.guardsquare.proguard") version proguardVersion
-        kotlin("jvm") version "2.0.0"
-        kotlin("plugin.serialization") version "2.0.0"
+        kotlin("jvm") version "2.3.10"
+        kotlin("plugin.serialization") version "2.3.10"
     }
 
 }
